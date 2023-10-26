078eed4f3fe8099208444ababb38c803b69bae4c
Felipe Dacal Fragoso <felipe@tollan.io>
2023-10-27 00:45:05 +0200

    Setup premake
    main

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "OThirteen"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+ 
+project "OThirteen"
+    location "OThirteen"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.22621.0"
+
+        defines
+        {
+            "OT_PLATFORM_WINDOWS",
+            "OT_BUILD_DLL",
+            "OTHIRTEEN_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "OT_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "OT_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "OT_DIST"
+        symbols "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "OThirteen/vendor/spdlog/include",
+        "OThirteen/src"
+    }
+
+    links
+    {
+        "OThirteen"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.22621.0"
+
+        defines
+        {
+            "OT_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "OT_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "OT_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "OT_DIST"
+        symbols "On"
+
